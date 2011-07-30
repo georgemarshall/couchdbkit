5ee80e73cec5ff6e436a13ec89af9dd2d6dd7f71
George Marshall <echosx@gmail.com>
2011-07-29 17:18:56 -0700

    Changed management commands to not accept args

--- couchdbkit/ext/django/management/commands/sync_couchdb.py
+++ couchdbkit/ext/django/management/commands/sync_couchdb.py
@@ -1,10 +1,11 @@
+from django.core.management.base import NoArgsCommand
 from django.db.models import get_apps
-from django.core.management.base import BaseCommand
+
 from couchdbkit.ext.django.loading import couchdbkit_handler
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
     help = 'Sync couchdb views.'
 
-    def handle(self, *args, **options):
+    def handle_noargs(self, **options):
         for app in get_apps():
             couchdbkit_handler.sync(app, verbosity=2)
--- couchdbkit/ext/django/management/commands/sync_finish_couchdb.py
+++ couchdbkit/ext/django/management/commands/sync_finish_couchdb.py
@@ -1,10 +1,11 @@
+from django.core.management.base import NoArgsCommand
 from django.db.models import get_apps
-from django.core.management.base import BaseCommand
+
 from couchdbkit.ext.django.loading import couchdbkit_handler
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
     help = 'Copy temporary design docs over existing ones'
 
-    def handle(self, *args, **options):
+    def handle_noargs(self, **options):
         for app in get_apps():
             couchdbkit_handler.copy_designs(app, temp='tmp', verbosity=2)
